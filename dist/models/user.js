7ad0470991ba52d3f709148f964b28bec8a8b529
jestorDev <torrele.09@gmail.com>
2022-03-30 16:18:46 -0500

    Created user data model

---
+++ dist/models/user.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const mongoose_1 = require("mongoose");
+const UserSchema = new mongoose_1.Schema({
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        lowercase: true,
+        trim: true,
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+exports.default = (0, mongoose_1.model)("User", UserSchema);
